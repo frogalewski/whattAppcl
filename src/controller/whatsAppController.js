20f4966e6c4ba76175c13161376de3e7278b5a39
frogalewski <frogalewski@gmail.com>
2022-10-03 21:31:21 -0300

    primeiro commit

--- src/controller/whatsAppController.js
+++ src/controller/whatsAppController.js
@@ -4,6 +4,7 @@ class WhatsAppController {
 
         console.log('WhatsAppController OK');
 
+        this.ElementsPrototype();
         this.loadElements();
     }
 
@@ -11,11 +12,35 @@ loadElements(){
 
 this.el = {};
 
-    document.querySelectorAll('[id]').forEach(element={
+    document.querySelectorAll('[id]').forEach(element=>{
 
-this.el['elements.id'] = elements;
+this.el[Format.getCamelCase(element.id)] = element;
 
     });
+
 }
 
-} 
+    elementsPrototype(){
+
+    Element.prototype.hide = function(){  
+    this.style.display = 'none';
+
+       }
+
+       Element.prototype.show = function () {  
+    this.style.display= 'block';
+
+       }
+
+       Element.prototype.toggle = function () {  
+    this.style.display = (this.style.display ==='none') ? 'block' : 'none'
+
+       }
+       Element.prototype.on = funcion (events, fn) {
+          events.split(' ').forEach(event=>{
+            this.addEventListener{event, fn};
+          });
+       }
+   }
+    }
+      
